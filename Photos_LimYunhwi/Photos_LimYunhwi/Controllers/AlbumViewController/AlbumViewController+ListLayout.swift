caff4a7b963052e4736a0f3eb774c5f80a9358b0
88yhtserof <88yhtserofg@gmail.com>
2024-08-16 16:16:47 +0900

    [MOVE] Album: ListLayout 코드 분리

---
+++ Photos_LimYunhwi/Photos_LimYunhwi/Controllers/AlbumViewController/AlbumViewController+ListLayout.swift
@@ -0,0 +1,68 @@
+//
+//  AlbumViewController+ListLayout.swift
+//  Photos_LimYunhwi
+//
+//  Created by 임윤휘 on 8/16/24.
+//
+
+import UIKit
+
+// MARK: CollectionView Layout
+extension AlbumViewController {
+    
+    func layout() -> UICollectionViewLayout {
+        let configuration = UICollectionViewCompositionalLayoutConfiguration()
+        configuration.interSectionSpacing = 20
+        return UICollectionViewCompositionalLayout(sectionProvider: sectionProviderHandler, configuration: configuration)
+    }
+    
+    func sectionProviderHandler( sectionIndex: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection {
+        guard let section = Section(rawValue: sectionIndex) else {
+            fatalError("Unknown Section")
+        }
+        switch section {
+        case .myAlbum:
+            return sectionForMyAlbum()
+        case .mediaTypes:
+            return sectionForMediaTypes(layoutEnvironment)
+        }
+    }
+    
+    func sectionForMyAlbum() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(0.48))
+        let nestedGroupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.48), heightDimension: .fractionalHeight(1.0))
+        let containerGroupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.95), heightDimension: .absolute(480))
+        
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let nestedGroup = NSCollectionLayoutGroup.vertical(layoutSize: nestedGroupSize, repeatingSubitem: item, count: 2)
+        nestedGroup.interItemSpacing = NSCollectionLayoutSpacing.fixed(10)
+        
+        let containerGroup = NSCollectionLayoutGroup.horizontal(layoutSize: containerGroupSize, repeatingSubitem: nestedGroup, count: 2)
+        containerGroup.interItemSpacing = NSCollectionLayoutSpacing.fixed(10)
+        
+        let section = NSCollectionLayoutSection(group: containerGroup)
+        section.orthogonalScrollingBehavior = .groupPaging
+        section.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 0, bottom: 0, trailing: 0)
+        section.boundarySupplementaryItems = [titleBoundarySupplementaryItem()]
+        
+        return section
+    }
+    
+    func sectionForMediaTypes(_ layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection {
+        var configuaration = UICollectionLayoutListConfiguration(appearance: .plain)
+        configuaration.showsSeparators = true
+        
+        let section = NSCollectionLayoutSection.list(using: configuaration, layoutEnvironment: layoutEnvironment)
+        section.boundarySupplementaryItems = [titleBoundarySupplementaryItem()]
+        section.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: -13, bottom: 0, trailing: 0)
+        
+        return section
+    }
+    
+    func titleBoundarySupplementaryItem() -> NSCollectionLayoutBoundarySupplementaryItem {
+        let titleSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                              heightDimension: .estimated(50))
+        return NSCollectionLayoutBoundarySupplementaryItem(layoutSize: titleSize, elementKind: titleElementKind, alignment: .top)
+    }
+}
--- Photos_LimYunhwi/Photos_LimYunhwi/Controllers/AlbumViewController/AlbumViewController.swift
+++ Photos_LimYunhwi/Photos_LimYunhwi/Controllers/AlbumViewController/AlbumViewController.swift
@@ -9,9 +9,17 @@ import UIKit
 
 class AlbumViewController: UIViewController {
     
+    enum Supplementary: String {
+        case title = "title-element-kind"
+        
+        var name: String {
+            self.rawValue
+        }
+    }
+    
     var dataSouce: DataSource!
     var snapshot: Snapshot!
-    private let titleElementKind = "title-element-kind"
+    let titleElementKind = "title-element-kind"
     
     private lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout())
 
@@ -61,65 +69,5 @@ private extension AlbumViewController {
         
         updateSnapshot()
         collectionView.dataSource = dataSouce
-    }
-}
-
-// MARK: CollectionView Layout
-private extension AlbumViewController {
-    
-    func sectionProviderHandler( sectionIndex: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection {
-        guard let section = Section(rawValue: sectionIndex) else {
-            fatalError("Unknown Section")
-        }
-        switch section {
-        case .myAlbum:
-            return sectionForMyAlbum()
-        case .mediaTypes:
-            return sectionForMediaTypes(layoutEnvironment)
-        }
-    }
-    
-    func layout() -> UICollectionViewLayout {
-        var configuration = UICollectionViewCompositionalLayoutConfiguration()
-        configuration.interSectionSpacing = 20
-        return UICollectionViewCompositionalLayout(sectionProvider: sectionProviderHandler, configuration: configuration)
-    }
-    
-    func sectionForMyAlbum() -> NSCollectionLayoutSection {
-        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(0.48))
-        let nestedGroupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.48), heightDimension: .fractionalHeight(1.0))
-        let containerGroupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.95), heightDimension: .absolute(480))
-        
-        let item = NSCollectionLayoutItem(layoutSize: itemSize)
-        
-        let nestedGroup = NSCollectionLayoutGroup.vertical(layoutSize: nestedGroupSize, repeatingSubitem: item, count: 2)
-        nestedGroup.interItemSpacing = NSCollectionLayoutSpacing.fixed(10)
-        
-        let containerGroup = NSCollectionLayoutGroup.horizontal(layoutSize: containerGroupSize, repeatingSubitem: nestedGroup, count: 2)
-        containerGroup.interItemSpacing = NSCollectionLayoutSpacing.fixed(10)
-        
-        let section = NSCollectionLayoutSection(group: containerGroup)
-        section.orthogonalScrollingBehavior = .groupPaging
-        section.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 0, bottom: 0, trailing: 0)
-        section.boundarySupplementaryItems = [titleBoundarySupplementaryItem()]
-        
-        return section
-    }
-    
-    func sectionForMediaTypes(_ layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection {
-        var configuaration = UICollectionLayoutListConfiguration(appearance: .plain)
-        configuaration.showsSeparators = true
-        
-        let section = NSCollectionLayoutSection.list(using: configuaration, layoutEnvironment: layoutEnvironment)
-        section.boundarySupplementaryItems = [titleBoundarySupplementaryItem()]
-        section.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: -13, bottom: 0, trailing: 0)
-        
-        return section
-    }
-    
-    func titleBoundarySupplementaryItem() -> NSCollectionLayoutBoundarySupplementaryItem {
-        let titleSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
-                                              heightDimension: .estimated(50))
-        return NSCollectionLayoutBoundarySupplementaryItem(layoutSize: titleSize, elementKind: titleElementKind, alignment: .top)
     }
 }
