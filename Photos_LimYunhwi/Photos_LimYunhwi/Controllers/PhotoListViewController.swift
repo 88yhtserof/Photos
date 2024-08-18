5e5589d8344a0e9a2dbfd67457704682f015d24d
88yhtserof <88yhtserofg@gmail.com>
2024-08-18 20:11:26 +0900

    [FEAT] PhotoList: 사진 목록 Layout 설정

--- Photos_LimYunhwi/Photos_LimYunhwi/Controllers/PhotoListViewController.swift
+++ Photos_LimYunhwi/Photos_LimYunhwi/Controllers/PhotoListViewController.swift
@@ -11,6 +11,7 @@ import Photos
 class PhotoListViewController: UIViewController {
     
     var assetCollection: PHAssetCollection
+    lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout())
     
     init(assetCollection: PHAssetCollection) {
         self.assetCollection = assetCollection
@@ -25,6 +26,37 @@ class PhotoListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        configureSubviews()
+        configureView()
+        configureConstratins()
+    }
+}
+
+//MARK: Configuration
+private extension PhotoListViewController {
+    func configureSubviews() {
+        collectionView.contentInset = UIEdgeInsets(top: 10, left: 10, bottom: 20, right: -10)
+    }
+    
+    func configureView() {
         view.backgroundColor = .white
+    }
+    
+    func configureConstratins() {
+        view.addPinnedSubview(collectionView, height: nil)
+    }
+}
+
+extension PhotoListViewController {
+    func layout() -> UICollectionViewLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.25), heightDimension: .fractionalHeight(1.0))
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalWidth(0.25))
+        
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        let section = NSCollectionLayoutSection(group: group)
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        
+        return layout
     }
 }
